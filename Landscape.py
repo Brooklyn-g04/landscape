125c698886a4dfa1dd8ae586d57ab48f20b13af5
100103067 <100103067@lubbockisd.net>
2019-09-27 15:11:02 -0500

    Initial commit

---
+++ Landscape.py
@@ -0,0 +1,13 @@
+import turtle
+
+Ross = turtle.Turtle()
+Rachel = turtle.Turtle()
+Chandler = turtle.Turtle()
+Monica = turtle.Turtle()
+Phoebe = turtle.Turtle()
+
+Ross.shape("turtle")
+Rachel.shape("turtle")
+Chandler.shape("turtle")
+Monica.shape("turtle")
+Phoebe.shape("turtle")
